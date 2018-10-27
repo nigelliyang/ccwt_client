9a724b7428018bfe0f6288682a6fbf6e05f626a4
xycfree@163.com <bingpoli123>
2018-10-27 18:00:14 +0800

    ccwt client init......

--- ccwt_client/ccwt_feed.py
+++ ccwt_client/ccwt_feed.py
@@ -2,4 +2,23 @@
 # -*- coding: utf-8 -*-
 # @Date    : 2018/10/27 17:18    @Author  : xycfree
 # @Descript: 
+from pyalgotrade.barfeed import dbfeed
+from pyalgotrade.barfeed import membf
+from pyalgotrade import bar
+from pyalgotrade.utils import dt
 
+
+class Feed(membf.BarFeed):
+    def __init__(self, frequency, dbConfig=None, maxLen=None):
+        super(Feed, self).__init__(frequency, maxLen)
+        self.__db = Database(dbConfig)
+
+    def barsHaveAdjClose(self):
+        return False
+
+    def getDatabase(self):
+        return self.__db
+
+    def loadBars(self, instrument, timezone=None, fromDateTime=None, toDateTime=None):
+        bars = self.__db.getBars(instrument, self.getFrequency(), timezone, fromDateTime, toDateTime)
+        self.addBarsFromSequence(instrument, bars)
