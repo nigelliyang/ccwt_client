126ca320b1ed6ac05218e9323996c5fd525bba19
xycfree@163.com <bingpoli123>
2018-11-28 14:55:46 +0800

    update 2

--- exchange/bitmex/main_1.py
+++ exchange/bitmex/main_1.py
@@ -7,6 +7,7 @@ from pyalgotrade.technical import cross
 from pyalgotrade import plotter
 from pyalgotrade.stratanalyzer import returns
 # from exchange.bitmex.liveApi.livebarfeed import LiveFeed
+from exchange.bitmex.liveApi.livebarfeed import LiveFeed
 from exchange.bitmex.liveApi.livebroker import LiveBroker
 from exchange.bitmex.bitmex import Bitmex
 from exchange.bitmex.bitmex_client import BitmexTradeClient as BitmexClient
@@ -24,64 +25,34 @@ COIN_TYPE = BitmexCoinType('btc', 'usdt')
 K_PERIOD = 60
 REQ_DELAY = 0
 
+from pyalgotrade import strategy
+from pyalgotrade.bar import Frequency
+from ccwt_client.ccwt_feed import Feed
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import rsi
+
+
+
+# 构建一个策略
 class MyStrategy(strategy.BaseStrategy):
     def __init__(self, feed, instrument, brk):
         super(MyStrategy, self).__init__(feed, brk)
-        self.__position = None
+        self.__rsi = rsi.RSI(feed[instrument].getCloseDataSeries(), 14)
+        self.__sma_rsi = ma.SMA(self.__rsi, 15)
+        self.__sma = ma.SMA(feed[instrument].getCloseDataSeries(), 2)
+        logger.info("__sma: {}, {}.".format(self.__sma, self.__sma[-1]))
+
         self.__instrument = instrument
-        # We'll use adjusted close values instead of regular close values.
-        self.__prices = feed[instrument].getPriceDataSeries()
-        self.__sma = {}
-        self.__sma[60] = ma.SMA(self.__prices, 60)
-        self.__sma[10] = ma.SMA(self.__prices, 10)
-        self.__sma[30] = ma.SMA(self.__prices, 30)
 
-    def getSMA(self, period):
-        return self.__sma[period]
 
-    def onEnterOk(self, position):
-        execInfo = position.getEntryOrder().getExecutionInfo()
-        logger.info("BUY at $%.2f %.4f" % (execInfo.getPrice(), execInfo.getQuantity()))
-
-    def onEnterCanceled(self, position):
-        logger.info("onEnterCanceled")
-        self.__position = None
-
-    def onExitOk(self, position):
-        execInfo = position.getExitOrder().getExecutionInfo()
-        logger.info("SELL at $%.2f" % (execInfo.getPrice()))
-        self.__position = None
-
-    def onExitCanceled(self, position):
-        # If the exit was canceled, re-submit it.
-        logger.info("onExitCanceled")
-        self.__position.exitMarket()
-
-    def onBars(self, bars):
-        # Wait for enough bars to be available to calculate a SMA.
+    def onBars(self, bars):  # 每一个数据都会抵达这里
         bar = bars[self.__instrument]
-        if self.getFeed().isHistory():
-            return
-        if self.__sma[60][-1] is None:
-            return
-        logger.info("onBars %s:%s: close:%.2f" % (self.__instrument, bar.getDateTimeLocal(), bar.getPrice()))
-
-        bar = bars[self.__instrument]
-
-        # If a position was not opened, check if we should enter a long position.
-        if self.__position is None:
-            if cross.cross_above(self.__sma[10], self.__sma[30]) > 0:
-                mbroker = self.getBroker()
-                shares = mbroker.getCash() / bar.getPrice() * 0.9
-                self.__position = self.enterLongLimit(self.__instrument, bar.getPrice(), shares, True)
-        # Check if we have to exit the position.
-        elif not self.__position.exitActive() and cross.cross_below(self.__sma[10], self.__sma[30]) > 0:
-            self.__position.exitLimit(bar.getPrice())
+        self.info("%s %s %s" % (bar.getClose(), self.__sma[-1], self.__sma_rsi[-1]))  # 我们打印输出收盘价与两分钟均线值
 
 
 def run_strategy():
     logger.info("-------START-------")
-    # feed = LiveFeed([COIN_TYPE], Frequency.MINUTE, REQ_DELAY)
+    feed = LiveFeed([COIN_TYPE], Frequency.MINUTE, REQ_DELAY)
     liveBroker = LiveBroker(COIN_TYPE, BitmexClient(COIN_TYPE))
     from exchange.bitmex.api_keys import API_KEY, API_SECRET
     _bitmex = Bitmex()
@@ -89,19 +60,19 @@ def run_strategy():
     frequency = bar.Frequency.MINUTE
     _bitmex.apiKey = API_KEY
     _bitmex.secret = API_SECRET
-    res = _bitmex.fetch_ticker(_symbol, params={"binSize": '5m', 'count': '100'})
-    print('res is len: {}'.format(res[0]))
-    feed = []
-    for row in res:
-        try:
-            feed.append(bar.BasicBar(datetime.strptime(datetime.strftime(
-                datetime.strptime(row.get('datetime'), "%Y-%m-%dT%H:%M:%S%sZ"), "%Y-%m-%d %H:%M:%S"),
-                "%Y-%m-%d %H:%M:%S"), row.get('open', 0) or row.get('preclose', 0), row.get('high', 0),
-                row.get('low', 0),
-                row.get('close', 0), row.get('volume', 0) or row.get('quoteVolume', 0), frequency)
-                )
-        except:
-            pass
+    # res = _bitmex.fetch_ticker(_symbol, params={"binSize": '5m', 'count': '100'})
+    # print('res is len: {}'.format(res[0]))
+    # feed = []
+    # for row in res:
+    #     try:
+    #         feed.append(bar.BasicBar(datetime.strptime(datetime.strftime(
+    #             datetime.strptime(row.get('datetime'), "%Y-%m-%dT%H:%M:%S%sZ"), "%Y-%m-%d %H:%M:%S"),
+    #             "%Y-%m-%d %H:%M:%S"), row.get('open', 0) or row.get('preclose', 0), row.get('high', 0),
+    #             row.get('low', 0),
+    #             row.get('close', 0), row.get('volume', 0) or row.get('quoteVolume', 0), frequency)
+    #             )
+    #     except:
+    #         pass
 
     myStrategy = MyStrategy(feed, _symbol, liveBroker)
     myStrategy.run()
